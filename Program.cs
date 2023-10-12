ec05757ace84376be1f30bac097cb6a6bae52380
Vasīlijs Černousovs <73379769+Chernousovs@users.noreply.github.com>
2023-10-12 10:25:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace BaiTELtask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonFilePath = "../../A_JSON_dati.json";
+            string xmlFilePath = "../../B_XML_dati.xml";
+
+            string text = File.ReadAllText(jsonFilePath);
+            var salesOrder = JsonSerializer.Deserialize<SalesOrder>(text);
+
+            StreamWriter writer = new StreamWriter(xmlFilePath);
+            salesOrder.ToXmlFile(writer);
+
+            Console.WriteLine("Check results in {0}", Path.GetFullPath(xmlFilePath));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SalesOrder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace BaiTELtask
+{
+    public class SalesOrder
+    {
+        [JsonPropertyName("order")]
+        public SalesOrderData Order { get; set; }
+
+        public void ToXmlFile(StreamWriter writer)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SalesOrder));
+            serializer.Serialize(writer, this);
+        }
+    }
+
+    public class SalesOrderData
+    {
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+        [JsonPropertyName("total_grams")]
+        public int TotalGrams { get; set; }
+        [JsonPropertyName("client_id")]
+        public string ClientId { get; set; }
+        [JsonPropertyName("items")]
+        public List<Item> Items { get; set; }
+    }
+
+    public class Item
+    {
+        [JsonPropertyName("product_id")]
+        public string ProductId { get; set; }
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+        [JsonPropertyName("sku")]
+        public string Sku { get; set; }
+    }
+}
